90dedf3e7a680f61cbfc0dd6917c2e46cc59fe03
erikandersoon0824 <bravehorse0824@gmail.com>
2024-03-20 20:36:37 +0900

    add news

--- src/pages/NewsList/NewsList.tsx
+++ src/pages/NewsList/NewsList.tsx
@@ -4,7 +4,7 @@ import Block from 'src/components/Block/Block';
 import DataTable from 'src/components/DataTable/DataTable';
 import { tableData, tableColumns } from 'src/components/DataTable/TableData';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NewsList() {
   return (
@@ -12,11 +12,11 @@ function NewsList() {
       <Block>
         <Block.Head className="wide-md">
           <Block.HeadContent>
-            <Link to="/news/add" style={{ textDecoration: 'none' }}>
+            <NavLink to="/news/add" style={{ textDecoration: 'none' }}>
               <Button sx={{ margin: 1 }} variant="contained" color="primary">
                 Add New One
               </Button>
-            </Link>
+            </NavLink>
           </Block.HeadContent>
         </Block.Head>
         <Card>
