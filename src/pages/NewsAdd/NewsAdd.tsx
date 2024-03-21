98add9bb4c5ce223eb98c4081f48f0ea37f67aa3
erikandersoon0824 <bravehorse0824@gmail.com>
2024-03-21 09:35:33 +0900

    add list revision

--- src/pages/NewsAdd/NewsAdd.tsx
+++ src/pages/NewsAdd/NewsAdd.tsx
@@ -1,10 +1,12 @@
 import { Helmet } from 'react-helmet-async';
 import PageTitle from 'src/components/PageTitle';
-import { useState } from 'react';
+import { useState, SyntheticEvent } from 'react';
 
 import PageTitleWrapper from 'src/components/PageTitleWrapper';
 import {
   Container,
+  Tab,
+  Tabs,
   Grid,
   Card,
   CardHeader,
@@ -42,19 +44,49 @@ const currencies = [
   }
 ];
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
 function Forms() {
   const [currency, setCurrency] = useState('EUR');
+  const [value, setValue] = useState(0);
 
   const handleChange = (event) => {
     setCurrency(event.target.value);
   };
 
-  const [value, setValue] = useState(30);
-
-  const handleChange2 = (event, newValue) => {
+  const handleTabChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
-
   return (
     <>
       <Helmet>
@@ -76,101 +108,159 @@ function Forms() {
           <Grid item xs={12}>
             <Card>
               <CardContent>
-                <Box
-                  component="form"
-                  sx={{
-                    '& .MuiTextField-root': { width: '100%', mb: 5 },
-                    '& .ql-container': { mb: 5 }
-                  }}
-                  noValidate
-                  autoComplete="off"
-                >
-                  <div>
-                    <CardHeader title="News Title(English)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter News Title"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="Short Description (English)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter Short Description"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="Content (English)" />
-                    <Quill placeholderValue="Hello world!" />
-                  </div>
-                  <div>
-                    <CardHeader title="Search For Slug (English)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter Sly"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="News Title (Swedish)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter News Title"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="Short Description (Swedish)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter Short Description"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="Content (Swedish)" />
-                    <Quill placeholderValue="Hello world!" />
-                  </div>
-                  <div>
-                    <CardHeader title="Search For Slug (Swedish)" />
-                    <TextField
-                      fullWidth
-                      id="news-title"
-                      label="Enter Sly"
-                      type="search"
-                    />
-                  </div>
-                  <div>
-                    <CardHeader title="Picture" />
-                    <div
-                      style={{ border: '1px solid grey', borderRadius: '7px' }}
+                <Box sx={{ width: '100%' }}>
+                  <Tabs
+                    variant="scrollable"
+                    scrollButtons="auto"
+                    textColor="primary"
+                    indicatorColor="primary"
+                    value={value}
+                    onChange={handleTabChange}
+                    aria-label="basic tabs example"
+                  >
+                    <Tab label="English" {...a11yProps(0)} />
+                    <Tab label="Swedish" {...a11yProps(1)} />
+                  </Tabs>
+                  <TabPanel value={value} index={0}>
+                    <Box
+                      component="form"
+                      sx={{
+                        '& .MuiTextField-root': { width: '100%', mb: 5 },
+                        '& .ql-container': { mb: 5 }
+                      }}
+                      noValidate
+                      autoComplete="off"
                     >
-                      <FileUpload iconName="files" />
-                    </div>
-                  </div>
-                  <div style={{ marginTop: '40px' }}>
-                    <Button
-                      sx={{ margin: '.625rem' }}
-                      variant="contained"
-                      color="success"
+                      <div>
+                        <CardHeader title="News Title" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter News Title"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Short Description" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter Short Description"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Content" />
+                        <Quill placeholderValue="Hello world!" />
+                      </div>
+                      <div>
+                        <CardHeader title="Search For Slug" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter Sly"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Picture" />
+                        <div
+                          style={{
+                            border: '1px solid grey',
+                            borderRadius: '7px'
+                          }}
+                        >
+                          <FileUpload iconName="files" />
+                        </div>
+                      </div>
+                      <div style={{ marginTop: '40px' }}>
+                        <Button
+                          sx={{ margin: '.625rem' }}
+                          variant="contained"
+                          color="success"
+                        >
+                          Save
+                        </Button>
+                        <Button
+                          sx={{ margin: '.625rem' }}
+                          variant="contained"
+                          color="primary"
+                        >
+                          Cancel
+                        </Button>
+                      </div>
+                    </Box>
+                  </TabPanel>
+                  <TabPanel value={value} index={1}>
+                    <Box
+                      component="form"
+                      sx={{
+                        '& .MuiTextField-root': { width: '100%', mb: 5 },
+                        '& .ql-container': { mb: 5 }
+                      }}
+                      noValidate
+                      autoComplete="off"
                     >
-                      Save
-                    </Button>
-                    <Button
-                      sx={{ margin: '.625rem' }}
-                      variant="contained"
-                      color="primary"
-                    >
-                      Cancel
-                    </Button>
-                  </div>
+                      <div>
+                        <CardHeader title="News Title" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter News Title"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Short Description" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter Short Description"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Content" />
+                        <Quill placeholderValue="Hello world!" />
+                      </div>
+                      <div>
+                        <CardHeader title="Search For Slug" />
+                        <TextField
+                          fullWidth
+                          id="news-title"
+                          label="Enter Sly"
+                          type="search"
+                        />
+                      </div>
+                      <div>
+                        <CardHeader title="Picture" />
+                        <div
+                          style={{
+                            border: '1px solid grey',
+                            borderRadius: '7px'
+                          }}
+                        >
+                          <FileUpload iconName="files" />
+                        </div>
+                      </div>
+                      <div style={{ marginTop: '40px' }}>
+                        <Button
+                          sx={{ margin: '.625rem' }}
+                          variant="contained"
+                          color="success"
+                        >
+                          Save
+                        </Button>
+                        <Button
+                          sx={{ margin: '.625rem' }}
+                          variant="contained"
+                          color="primary"
+                        >
+                          Cancel
+                        </Button>
+                      </div>
+                    </Box>
+                  </TabPanel>
                 </Box>
               </CardContent>
             </Card>
